acf4da4df8b4b420494b9e911a95a41aee9b0869
Joseph Caruana <jcaruana614@gmail.com>
2020-04-16 11:38:37 -0700

    n*2

---
+++ addStr.js
@@ -0,0 +1,9 @@
+var addStrings = function (num1, num2) {
+    // use unary plus operator
+    total = +num1 + +num2
+    return total.toString();
+};
+
+result = addStrings("550", "350");
+
+console.log(typeof(result))
