2f009ef4d662904991632715c4a12707dc65fecb
Joseph Caruana <jcaruana614@gmail.com>
2020-05-12 12:10:13 -0700

    recursion

---
+++ More Algos/recursion/factorial.js
@@ -0,0 +1,9 @@
+// factorial is n-1 * n -1 until it reaches 0
+
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+console.log(factorial(5));  
