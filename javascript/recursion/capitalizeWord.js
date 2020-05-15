a161f3a056b411fd75ae59c402a315346d265dc9
Joseph Caruana <jcaruana614@gmail.com>
2020-05-15 12:59:18 -0700

    capitalizewords nested evensum

---
+++ javascript/recursion/capitalizeWord.js
@@ -0,0 +1,30 @@
+// return a new array of capitalzied words
+
+function capitalizeWords(words, capWords) {
+    
+    if (words.length === 0) {
+        return capWords
+    }
+    // capWords = [];
+    let push = words[0].toUpperCase();
+    capWords.push(push);
+    return capitalizeWords(words.slice(1), capWords);
+  }
+  
+
+  console.log(capitalizeWords(['i', 'am', 'learning', 'recursion'], []));
+    // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+// capitalizeWords(['i', 'am', 'learning'])
+//  capWords = []
+//        push = 'I'
+//            capWords = ['I']
+                // return ['I] + capitalizeWords(['am', 'learning', 'recursion'])
+                //  capWords = []
+                //      push = 'AM'
+                //          capWords = ['AM']
+                            //  return ['AM'] + capitalizeWords(['learning'])
+                            //      capWords = []
+                            //          push = 'LEARNING'
+                            //              capWords = ['LEARNING']
+                            //                  return ['LEARNING'] + capitalizeWords([])
---
+++ javascript/recursion/nestedEvenSum.js
@@ -0,0 +1,38 @@
+// write a function that finds all the nested even numberes in an nested object
+
+function nestedEvenSum (dict) {
+    let count = 0
+    for(let key in dict) {
+       if(typeof dict[key] === 'object'){
+           count += nestedEvenSum(dict[key])
+       } else if (typeof dict[key] === 'number' && dict[key] % 2 == 0) {
+            count += dict[key]
+       }
+
+    }
+    return count
+  }
+  
+  
+  var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+  
+  var obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+  };
+  
+  console.log(nestedEvenSum(obj1)); // 6
+  console.log(nestedEvenSum(obj2)); // 10
