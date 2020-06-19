c23ca05b635dde00bf72caa53d4220e8d37e9ff6
Joseph Caruana <jcaruana614@gmail.com>
2020-06-19 15:04:21 -0700

    doublinkylist

---
+++ javascript/dataStructures/doubleLinkedList.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    static push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++
+        return this;
+    }
+    static pop(val) {
+        if (!this.head) return undefined;
+        let temp = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+            
+        }
+    }
+}
