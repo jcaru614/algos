c9d77b8761553af10f3538b94ddc8d81fe10cf89
Joseph Caruana <jcaruana614@gmail.com>
2022-11-20 17:13:54 -0600

    .DS_Store banished!

---
+++ course/problem_solving_patterns/challenges/areThereDuplicates.js
@@ -0,0 +1,38 @@
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+const areThereDuplicatesFC = (...items) => {
+  let possibleDuplicates = {};
+  for (const item of items) {
+    if (possibleDuplicates[item]) return true;
+    else possibleDuplicates[item] = 1;
+  }
+  return false;
+};
+
+const areThereDuplicatesMP = (...items) => {
+  let sortedItems = items.sort();
+  console.log(sortedItems);
+  let left = 0;
+  let right = sortedItems.length - 1;
+  while (left <= right) {
+    if (sortedItems[left] !== sortedItems[left + 1]) {
+      left += 1;
+    } else if (sortedItems[right] !== sortedItems[right - 1]) {
+      right += 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Examples:
+
+console.log(areThereDuplicatesFC(1, 2, 3)); // false
+console.log(areThereDuplicatesFC(1, 2, 2)); // true
+console.log(areThereDuplicatesFC("a", "b", "c", "a")); // true
+
+console.log(areThereDuplicatesMP(3, 1, 2, 5, 7)); // false
+console.log(areThereDuplicatesMP(2, 1, 2, 2, 5, 5, 22, 9)); // true
+console.log(areThereDuplicatesMP("a", "b", "c", "a", "d", "t", "y", "f")); // true
