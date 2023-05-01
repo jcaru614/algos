29fe08026b020b11cabb1fbc8a15f51ef651058b
jcaru614 <joseph.1.caruana@gm.com>
2023-05-01 14:13:59 -0500

    common characters

--- algoExport/strings/commonCharacters.js
+++ algoExport/strings/commonCharacters.js
@@ -1,5 +1,48 @@
 function commonCharacters(strings) {
-	
-	return [];
+  let stringsLength = strings.length;
+  if (stringsLength === 1) {
+    return strings;
+  }
+  let charFrequency = {};
+  let flatStrings = strings.join("");
+  let commonChars = [];
+
+  for (const item in flatStrings) {
+    let char = flatStrings[item];
+    charFrequency[char] = charFrequency[char] ? (charFrequency[char] += 1) : 1;
+  }
+  for (const key in charFrequency) {
+    if (charFrequency[key] >= stringsLength) {
+      commonChars.push(key);
+    }
+  }
+  return commonChars;
 }
-console.log(commonCharacters(['abc', 'bcd', 'cbad']));
+
+function commonCharactersWithMap(strings) {
+  let stringsLength = strings.length;
+  if (stringsLength === 1) {
+    return strings;
+  }
+  let charFrequency = new Map();
+  let flatStrings = strings.join("");
+  let commonChars = [];
+
+  for (const item in flatStrings) {
+    let char = flatStrings[item];
+    charFrequency.set(
+      char,
+      charFrequency.has(char) ? charFrequency.get(char) + 1 : 1
+    );
+  }
+  for (const [key, value] of charFrequency.entries()) {
+    if (value >= stringsLength) {
+      commonChars.push(key);
+    }
+  }
+
+  return commonChars;
+}
+
+console.log(commonCharacters(["abc", "bcd", "cbad"]));
+console.log(commonCharactersWithMap(["*abcd", "def*", "******d*****"]));
