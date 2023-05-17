f7b9a74db919741e3ffad5b9a5b96d1be5350de7
jcaru614 <joseph.1.caruana@gm.com>
2023-05-17 16:42:13 -0500

    it does work

--- algoExport/arrays/smallestDifference.js
+++ algoExport/arrays/smallestDifference.js
@@ -1,30 +1,41 @@
 function smallestDifference(arrayOne, arrayTwo) {
   arrayOne.sort((a, b) => a - b);
   arrayTwo.sort((a, b) => a - b);
-  console.log(arrayOne, arrayTwo);
+
   let i = 0,
     j = 0,
-    minDiff = [arrayOne[i], arrayTwo[j]];
+    currentDiff = [arrayOne[i], arrayTwo[j]];
+
   while (i < arrayOne.length && j < arrayTwo.length) {
     if (Math.abs(arrayOne[i] - arrayTwo[j]) === 0) {
       return [arrayOne[i], arrayTwo[j]];
-    } else if (Math.min(arrayOne[i], arrayTwo[j]) === arrayOne[i]) {
-      minDiff =
-        Math.abs(minDiff[0] - minDiff[1]) > Math.abs(arrayOne[i] - arrayTwo[j])
+    }
+
+    if (Math.min(arrayOne[i], arrayTwo[j]) === arrayOne[i]) {
+      currentDiff =
+        Math.abs(arrayOne[i] - arrayTwo[j]) <
+        Math.abs(currentDiff[0] - currentDiff[1])
           ? [arrayOne[i], arrayTwo[j]]
-          : minDiff;
+          : currentDiff;
 
       i++;
     } else {
-      minDiff =
-        Math.abs(minDiff[0] - minDiff[1]) > Math.abs(arrayOne[i] - arrayTwo[j])
+      currentDiff =
+        Math.abs(arrayOne[i] - arrayTwo[j]) <
+        Math.abs(currentDiff[0] - currentDiff[1])
           ? [arrayOne[i], arrayTwo[j]]
-          : minDiff;
+          : currentDiff;
 
       j++;
     }
   }
-  return minDiff;
+  return currentDiff;
 }
-console.log(smallestDifference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]));
-// [ -1, 3, 5, 10, 20, 28 ] [ 15, 17, 26, 134, 135 ]
+// console.log(smallestDifference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]));
+
+console.log(
+  smallestDifference(
+    [10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123],
+    [-1441, -124, -25, 1014, 1500, 660, 410, 245, 530]
+  )
+);
