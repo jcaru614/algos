950e4485f232aa810d9d207480483233a518c2d1
jcaru614 <joseph.1.caruana@gm.com>
2023-05-22 21:07:58 -0500

    monotonic

---
+++ algoExport/arrays/longestPeak.js
@@ -0,0 +1,12 @@
+function longestPeak(array) {
+  let peakCount = 0;
+  let center = 1;
+  let left = center - 1;
+  let right = center + 1;
+  while (center < array.length - 1) {
+    
+  }
+  return peakCount;
+}
+
+console.log(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]));
